69ad70a123a08bb2d7db7563bd8d99658e2ffe49
Nikunj <6018.nike@gmail.com>
2022-12-29 13:47:54 +0530

    Deployed finished

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const dotenv = require('dotenv');
+const http = require('http');
+
 
 dotenv.config({ path: 'config.env' });
 
 const app = require('./app');
 
+const Server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+Server.listen(PORT, () => {
     console.log(`Server connected on port ${PORT} http://localhost:4000/`);
 });
